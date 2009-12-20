31ab7639db258b99e98c6287229211bc9465411b
Morgan Veyret <patzy@appart.kicks-ass.net>
2009-12-20 02:43:13 +0100

    Added context switch test.

--- test.lisp
+++ test.lisp
@@ -1,6 +1,6 @@
 (defpackage :glop-test
   (:use #:cl)
-  (:export #:test-manual-create #:test-with-window))
+  (:export #:test-manual-create #:test-multiple-contexts #:test-with-window))
 
 (in-package #:glop-test)
 
@@ -32,9 +32,28 @@
     (loop while (glop:dispatch-events win :blocking nil) do
          (gl:clear :color-buffer)
          (gl:flush)
+         (format t "Swapping buffers~%")
          (glop:swap-buffers win))
     (glop:destroy-window win)))
 
+(defun test-multiple-contexts ()
+  (let ((win (glop:create-window "Glop test window" 800 600))
+        (time (get-internal-real-time)))
+    (format t "Created window: ~S~%" win)
+    (gl:clear-color 0.3 0.3 1.0 0)
+    (let ((ctx1 (glop::window-gl-context win))
+          (ctx2 (glop:create-gl-context win :make-current t)))
+      (gl:clear-color 1.0 0.3 0.3 0)
+      (loop while (glop:dispatch-events win :blocking nil) do
+           (gl:clear :color-buffer)
+           (gl:flush)
+           (glop:swap-buffers win)
+         when (> (- (get-internal-real-time) time) (* 2.0 internal-time-units-per-second))
+         do (progn (setf time (get-internal-real-time))
+                   (if (eql (glop::window-gl-context win) ctx1)
+                       (glop:attach-gl-context win ctx2)
+                       (glop:attach-gl-context win ctx1)))))
+    (glop:destroy-window win)))
 
 (defun test-with-window ()
   (glop:with-window (win "Glop test window" 800 600)
