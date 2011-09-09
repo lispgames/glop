1171c3446c6fcf43bb7f292f8c4397ef06c25424
Jens Thiede <jensthiede@gmail.com>
2011-09-09 09:26:37 +0200

    Working on LIST-VIDEO-MODES. CFArrayRefs don't work yet.

--- src/osx/package.lisp
+++ src/osx/package.lisp
@@ -1,3 +1,19 @@
-(defpackage #:glop-osx
-  (:use :cl :cffi)
-  (:export))
+(defpackage #:glop-core-foundation
+  (:use #:cl #:cffi)
+  (:nicknames #:glop-cf)
+  (:export #:string-length
+           #:string-c-string
+           #:string-lisp-string
+           #:array-count
+           #:array-values
+           #:lisp-array-values))
+
+(defpackage #:glop-quartz
+  (:use :cl :cffi :glop-cf)
+  (:export #:main-display-id
+           #:copy-display-mode
+           #:copy-all-display-modes
+           #:mode-width
+           #:mode-height
+           #:mode-rate
+           #:mode-pixel-encoding))
