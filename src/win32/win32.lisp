c2cd3abac3620e66a49e409a1928f129385b50f9
Morgan Veyret <patzy@oxyde.org>
2011-10-23 21:37:57 +0200

    Removed ugly WM_SIZE hack.
    Now we're sending resize event on WM_EXITRESIZEMOVE.
    Still not completely right but better.

--- src/win32/win32.lisp
+++ src/win32/win32.lisp
@@ -576,8 +576,7 @@
 
 
 (let ((last-x 0)
-      (last-y 0)
-      (from-configure nil))
+      (last-y 0))
   (defcallback window-proc :long ((wnd hwnd) (msg :uint) (w-param wparam) (l-param lparam))
      (let ((msg-type (foreign-enum-keyword 'msg-type msg :errorp nil)))
        (case msg-type
@@ -596,13 +595,25 @@
                                                  :dx (- low last-x) :dy (- high last-y)))
               (setf last-x low last-y high))
             (return-from window-proc 0)))
-         (:wm-paint
-          ;; XXX: this is an ugly hack but WM_SIZE acts strangely...
+         (:wm-size
+          (when %window% ;; XXX: WM_SIZE is called on window creation when %window% is nil ...
+            (glop::%update-geometry %window% (glop:window-x %window%) (glop:window-y %window%)
+                                    (low-word l-param) (high-word l-param)))
+          (return-from window-proc 0))
+         (:wm-move
+          (when %window% ;; XXX: WM_MOVE is called on window creation when %window% is nil ...
+            (multiple-value-bind (x y w h) (get-window-rect wnd)
+              (glop::%update-geometry %window% x y ;;(low-word l-param) (high-word l-param)
+                                      (glop:window-width %window%) (glop:window-height %window%)))
+            (return-from window-proc 0)))
+         (:wm-exit-size-move
+          (setf %event% (make-instance 'glop:resize-event
+                                       :width (glop:window-width %window%)
+                                       :height (glop:window-height %window%)))
+            (return-from window-proc 0))
+         (:wm-paint ;; XXX: we need to call defaut windowproc too here
           (multiple-value-bind (x y width height) (get-client-rect wnd)
-            (setf %event% (glop::make-instance (if from-configure
-                                                   (progn (setf from-configure nil)
-                                                          'glop:resize-event)
-                                                   'glop:expose-event)
+            (setf %event% (glop::make-instance 'glop:expose-event
                                                :width width :height height))))
          (:wm-lbutton-down
           (set-capture wnd)
@@ -657,34 +668,18 @@
                                              :button (if (> w-param 0)
                                                          4 5)))
           (return-from window-proc 0))
-         (:wm-size
-          ;; XXX: other part of above ugly hack...
-          ;; Not sure why but it looks WM_SIZE doesn't get out of window-proc
-          ;; until mouse button is released...
-          ;; Maybe we can use WM_EXITSIZEMOVE and send :configure event only at the end
-          ;; of the window resize while still sending a proper expose event...
-          ;; With this hack it seems that WM_PAINT is handled directly thus overwriting
-          ;; our glop :configure event with a :expose event...weird
-          (setf from-configure t)
-          (when %window% ;; XXX: WM_SIZE is called on window creation when %window% is nil ...
-            (glop::%update-geometry %window% (glop:window-x %window%) (glop:window-y %window%)
-                                    (low-word l-param) (high-word l-param)))
-          (return-from window-proc 0))
-         (:wm-move
-          (when %window% ;; XXX: WM_MOVE is called on window creation when %window% is nil ...
-            (multiple-value-bind (x y w h) (get-window-rect wnd)
-              (glop::%update-geometry %window% x y ;;(low-word l-param) (high-word l-param)
-                                      (glop:window-width %window%) (glop:window-height %window%)))
-            (return-from window-proc 0)))
          (:wm-show-window
           (setf %event% (glop::make-instance (if (zerop w-param)
                                                  'glop:visibility-unobscured-event
                                                  'glop:visibility-obscured-event)))
           (return-from window-proc 0))
          (:wm-set-focus
+          (setf %event% (make-instance 'glop:focus-in-event))
+          (return-from window-proc 0))
+         (:wm-kill-focus
+          (setf %event% (make-instance 'glop:focus-out-event))
           (return-from window-proc 0)))
-       ;; (:wm-sys-command
-       ;;  (format t "WM_SYSCOMMAND~%")))
+       ;; Pass unhandled messages to default windowproc
        (%def-window-proc wnd msg w-param l-param))))
 
 (defcfun ("RegisterClassA" %register-class) :int16
